aff9da77a340e463495c264b0e7f149de1328f67
czincz <czincz@legion.localdomain>
2023-05-11 01:47:32 +0200

    fixed

--- pyinstaller/linux_onefile_color/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pyinstaller.exe --collect-all ytmusicapi --collect-all requests --collect-all colorama--add-binary 'yt-dlp.exe:.' --onefile ytmusic-dl-color.py
--- pyinstaller/windows_build/BUILDCOMMAND.sh
+++
@@ -1,1 +0,0 @@
-wine /home/czincz/.wine/drive_c/users/czincz/AppData/Local/Programs/Python/Python311/Scripts/pyinstaller.exe --collect-all ytmusicapi --collect-all requests --add-binary 'yt-dlp.exe;.' --add-binary 'ffmpeg.exe;.' --onefile ytmusic-dl.py
