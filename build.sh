b91da0948def675e49f95d621c2ce99b42a26e54
czincz <czincz@legion.localdomain>
2023-05-16 00:27:29 +0200

    fixed path problem

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ red=$(tput setaf 1)
 mag=$(tput setaf 5)
 cya=$(tput setaf 6)
 cnc=$(tput sgr0)
+pipth="$(python -m site --user-base)/bin/pyinstaller"
 
 echo "This script requires$red pip$grn (the package installer for Python)$cnc and$red wget$grn (The non-interactive network downloader)$cnc programs."
 echo "Install them from your GNU/Linux distribution repositories.$cnc"
@@ -24,11 +25,10 @@ select REPLY in EN PL CANCEL; do
     esac
 done
 
-
 wget 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp' -O yt-dlp-bin
 chmod +x yt-dlp-bin
 pip install -r "./src/requirements.txt" --user
-~/.local/bin/pyinstaller --distpath='./bin' --collect-all ytmusicapi --collect-all requests --collect-all colorama --add-binary 'yt-dlp-bin:.' --onefile --name ytmusic-dl "./src/"$vrs
+$pipth --distpath='./bin' --collect-all ytmusicapi --collect-all requests --collect-all colorama --add-binary 'yt-dlp-bin:.' --onefile --name ytmusic-dl "./src/"$vrs
 rm -f yt-dlp-bin
 rm -f *.spec
 rm -r build
